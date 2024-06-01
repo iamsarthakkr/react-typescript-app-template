bdeb9c3e49735277ed41b517e4493cf9656e485c
sarthak kumar <sarthakkumar10151@gmail.com>
2024-06-01 16:50:34 +0530

    Folder structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+import { Center } from "../components";
+
+const AppContainer = styled(Center)`
+   font-weight: 500;
+   font-size: 1.4em;
+`;
 
 export const App = () => {
-   return <div>Hello World</div>;
+   return <AppContainer>Hello World</AppContainer>;
 };
